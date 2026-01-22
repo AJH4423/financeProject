d9b5e40e849a82d472ce921804ccf0829ab67cbd
AJH4423 <anjunho12@naver.com>
2026-01-22 16:40:04 +0900

    feat: 깃허브 연동

---
+++ backend/src/test/java/JHJS/demo/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package JHJS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
